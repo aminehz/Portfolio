af777a9fb4073e0cb1c1e2ed07d096da35b035d8
aminehz <mohamedamin.harzallah@gmail.com>
2024-04-01 10:53:56 +0100

    deploy

---
+++ src/data/aboutMeData.js
@@ -0,0 +1,6 @@
+export const aboutMeData = [
+	{
+		id: 1,
+		bio: "My name is Med Amine HARZALLAH , I am a student in the 3rd year of engineering ,specializing in Computer Science at Polytechnic Private University of Sousse.My specialization is in software engineering with a focus on Artificial Intelligence (AI) and Big Data."},
+	
+];
---
+++ src/data/clientsData.js
@@ -0,0 +1,99 @@
+// Import images
+import AmazonImage from '../images/brands/amazon_gray.png';
+import SonyImage from '../images/brands/sony_gray.png';
+import AdidasImage from '../images/brands/adidas_gray.png';
+import FilaImage from '../images/brands/fila_gray.png';
+import NBImage from '../images/brands/nb_gray.png';
+import SamsungImage from '../images/brands/samsung_gray.png';
+import CanonImage from '../images/brands/canon_gray.png';
+import PumaImage from '../images/brands/puma_gray.png';
+import Python from "../images/python.jpg";
+import nodesjs from "../images/nodejs.png";
+import powerbi from "../images/powerbi.png";
+import react from "../images/react.png";
+import springboot from "../images/springboot.png";
+import talend from "../images/talend.png";
+import github from "../images/github.png";
+import intelij from "../images/intellij.png";
+import mongodb from "../images/mongodb.png";
+import postgresql from "../images/postgresql.png";
+import postman from "../images/postman.png";
+import pycharm from "../images/pycharm.png";
+import vscode from "../images/vsCode.png";
+import machine from "../images/machine learning.png";
+
+
+export const clientsHeading = 'Professional Skillset';
+
+export const clientsData = [
+	{
+		id: 1,
+		title: 'Python',
+		img: Python,
+	},
+	{
+		id: 2,
+		title: 'powerbi',
+		img: powerbi,
+	},
+	{
+		id: 3,
+		title: 'talend',
+		img: talend,
+	},
+	{
+		id: 3,
+		title: 'machine learning',
+		img: machine,
+	},
+	{
+		id: 4,
+		title: 'react',
+		img: react,
+	},
+	{
+		id: 5,
+		title: 'spring boot',
+		img: springboot,
+	},
+	{
+		id: 6,
+		title: 'nodejs',
+		img: nodesjs,
+	},
+	{
+		id: 7,
+		title: 'github',
+		img: github,
+	},
+	{
+		id: 8,
+		title: 'pycharm',
+		img: pycharm,
+	},
+	{
+		id: 8,
+		title: 'intellij',
+		img: intelij,
+	},
+	{
+		id: 8,
+		title: 'postgresql',
+		img: postgresql,
+	},
+	{
+		id: 8,
+		title: 'mongodb',
+		img: mongodb,
+	},
+	{
+		id: 8,
+		title: 'postman',
+		img: postman,
+	},
+	{
+		id: 8,
+		title: 'vs code',
+		img: vscode,
+	},
+];
